c74bd8b84b6f6bc1fb9da58cb422f8ce7b386216
Ridwanur504 <ridwanur196@gmail.com>
2023-02-20 10:37:47 +0600

    auth complete

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+    public function login(Request $request)
+    {
+        try {
+            $credentials = $request->validate([
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                return redirect()->intended('hello');
+            }
+            return redirect()->route('login')->withErrors('These credentials do not match our records.');
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+    public function logout(Request $request)
+    {
+        try {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect()->intended('/');
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+
+    }
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+    public function register(Request $request)
+    {
+        // dd('gygygygyg');
+        try {
+
+            $validatedData = $request->validate([
+                'name' => 'required|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:8',
+            ]);
+            //dd($validatedData['name']);
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt($validatedData['password'])
+            ]);
+
+            Auth::login($user);
+
+            return redirect()->intended('hello');
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
